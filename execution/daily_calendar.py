2fa9cf684962740d85f0213227bf0260c49f8de8
sisyphe10 <kts77775@gmail.com>
2026-02-05 14:30:53 +0900

    Add '옥쥬와 빵빵이' and '운용 본부' calendars

--- execution/daily_calendar.py
+++ execution/daily_calendar.py
@@ -50,6 +50,7 @@ def get_today_events():
     calendars_to_check = {
         'kts77775@gmail.com': '메인 캘린더',
         'a49c912f9e11c6e050c873312ae00a314e45dc075540c86cf428c9921fcbc20c@group.calendar.google.com': '옥쥬와 빵빵이',
+        'h7u3p3bs2tva7ki3e2up0tg30o@group.calendar.google.com': '운용 본부',
         # 다른 캘린더 ID를 여기에 추가
         # 예: 'abcd1234@group.calendar.google.com': '투자 활동',
     }
